57f427815e36e97328d1ebc1805044fd717da65e
Guilherme <guibma@hotmail.com>
2023-11-05 17:29:50 -0300

    ac

--- AC4/AC04/src/Ebook.java
+++
@@ -1,8 +0,0 @@
-public class Ebook extends Livro{
-    String formato;
-
-    public Ebook(String titulo, String autor, String formato) {
-        super(titulo, autor);
-        this.formato = formato;
-    }
-}
